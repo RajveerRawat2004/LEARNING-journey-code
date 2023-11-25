e2bbeaa058d40dd85ed8f30f29aad8b50dfa1f86
RajveerRawat2004 <rajveerrawat88390@gmail.com>
2023-11-25 20:03:20 +0530

    update with CSS part 4 CODE delta3.0

---
+++ Youtube Apna college/java Script Apna CollegeYT/class_1st/script.js
@@ -0,0 +1,12 @@
+
+// console.log("rajveer rawat is live")
+// console.log("at Apna college")
+// console.log("learning javaScript")
+
+fullname ="rajveer rawat";
+age = 35;
+y = 57;
+a = null;
+x = undefined;
+console.log("fullname");
+console.log("y");
