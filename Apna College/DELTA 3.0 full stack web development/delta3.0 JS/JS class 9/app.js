231b89f4bfbaa2ddc7e6b59c040c22d18e543fac
RajveerRawat2004 <rajveerrawat88390@gmail.com>
2023-12-21 16:50:40 +0530

    JS class 9 added

---
+++ Apna College/DELTA 3.0 full stack web development/delta3.0 JS/JS class 9/app.js
@@ -0,0 +1,8 @@
+// let newImg = document.getElementsByClassName("oldImg");
+
+// for (let i=0 ; i< newImg.length; i++){
+//     newImg[i].src = "assets/creation_1.png";
+//     console.log(`you have changed img of index ${i}`);
+// }
+
+// ASSIGNMENT QUESTIONS
