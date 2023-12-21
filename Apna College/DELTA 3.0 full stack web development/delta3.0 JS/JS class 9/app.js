7cc15c1d6a0349fb8d10a3fd255caf4be6497d5d
RajveerRawat2004 <rajveerrawat88390@gmail.com>
2023-12-21 17:01:06 +0530

    some name changed

--- Apna College/DELTA 3.0 full stack web development/delta3.0 JS/JS class 9/app.js
+++ Apna College/DELTA 3.0 full stack web development/delta3.0 JS/JS class 9/app.js
@@ -1,8 +1,11 @@
 // let newImg = document.getElementsByClassName("oldImg");
 
-// for (let i=0 ; i< newImg.length; i++){
-//     newImg[i].src = "assets/creation_1.png";
-//     console.log(`you have changed img of index ${i}`);
+// for (let i = 0; i < newImg.length; i++) {
+//   newImg[i].src = "assets/creation_1.png";
+//   console.log(`you have changed img of index ${i}`);
 // }
 
-// ASSIGNMENT QUESTIONS
+// practice Question -------------------------------------------------------------
+
+
+// ASSIGNMENT QUESTIONS -------------------------------------------------------
